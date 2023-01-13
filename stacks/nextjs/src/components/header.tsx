aa2d25875b2a3750a4cc39e79a4871d454f9b881
Thiago Thalisson <atomic-feast@hotmail.com>
2023-01-13 12:57:32 -0300

    Added radix navigation menu

--- stacks/nextjs/src/components/header.tsx
+++ stacks/nextjs/src/components/header.tsx
@@ -4,27 +4,33 @@ import { AiFillShopping } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
 
 import Image from 'next/image'
+import Navigation from '../components/navigation'
 
 export function Header() {
     return (
         <header className="header">
-            <Image src="/../public/LogoDark.png" width="20" height="20" alt="Logo Dark" />
-            <nav className="navigation">
+            <div className="header-wrapper">
+            <Navigation />
+            <a href="#">
+                <Image className="logo" src="/../public/LogoDark.png" width="25" height="25" alt="Logo Dark" />
+            </a>
+            <nav className="mobile-navigation">
                 <ul role="list">
-                    <a className="list-item" href="#home">
-                        <AiFillHome className="icon"/><li>Home</li>
+                    <a className="list-link" href="#home">
+                        <AiFillHome className="icon"/><li className="list-item">Home</li>
                     </a>
-                    <a className="list-item" href="#about">
-                        <AiOutlineUser className="icon"/><li>About Us</li>
+                    <a className="list-link" href="#about">
+                        <AiOutlineUser className="icon"/><li className="list-item">About Us</li>
                     </a>
-                    <a className="list-item" href="#contact">
-                        <AiOutlineContacts className="icon"/><li>Contact</li>
+                    <a className="list-link" href="#contact">
+                        <AiOutlineContacts className="icon"/><li className="list-item">Contact</li>
                     </a>
-                    <a className="list-item" href="#products">
-                        <AiFillShopping className="icon"/><li>Products</li>
+                    <a className="list-link" href="#products">
+                        <AiFillShopping className="icon"/><li className="list-item">Products</li>
                     </a>
                 </ul>
             </nav>
+            </div>
         </header>
     )
 
